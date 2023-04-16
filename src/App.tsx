08259d89ad6c09a90a52a87dd26680ebcbb38d56
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2023-04-16 15:10:14 +0500

    generated binary/hex outputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 type Selected = {
   rotation: number;
@@ -7,14 +7,56 @@ type Selected = {
 
 function App() {
   const [selected, setSelected] = useState<Selected[]>([]);
-  let list: (Number | String)[] = ['','','','','','','','',''];
+  const [bin1, setBin1] = useState<String[]>([]);
+  const [bin2, setBin2] = useState<String[]>([]);
+  const [hex1, setHex1] = useState<String>("");
+  const [hex2, setHex2] = useState<String>("");
+  let list: (Number | String)[] = [];
   for (let i = 1; i <= 16; i++) {
     list.push(i);
   }
   let rotations: Number[] = [];
-  for (let i = 0; i < 360; i += 3) {
+  for (let i = 0; i < 120; i += 1) {
     rotations.push(i);
   }
+
+  useEffect(() => {
+    if (selected.length > 0) {
+      let bin1arr: String[] = [];
+      let bin2arr: String[] = [];
+      let hex1str: String = "";
+      let hex2str: String = "";
+      for (let i = 0; i < 120; i++) {
+        let subArr1: Number[] = [];
+        let subArr2: Number[] = [];
+        for (let j = 0; j < 8; j++) {
+          if (selected.some((item) => item.rotation === i && item.item === j)) {
+            subArr1.push(1);
+          } else {
+            subArr1.push(0);
+          }
+        }
+        for (let j = 8; j < 16; j++) {
+          if (selected.some((item) => item.rotation === i && item.item === j)) {
+            subArr2.push(1);
+          } else {
+            subArr2.push(0);
+          }
+        }
+        bin1arr.push(subArr1.join(""));
+        bin2arr.push(subArr2.join(""));
+        subArr1.reverse();
+        subArr2.reverse();
+        hex1str += parseInt(subArr1.join(""), 2).toString(16);
+        hex2str += parseInt(subArr2.join(""), 2).toString(16);
+      }
+      setBin1(bin1arr);
+      setBin2(bin2arr);
+      setHex1(hex1str);
+      setHex2(hex2str);
+    }
+  }, [selected]);
+
   return (
     <div className="relative flex items-center justify-center h-screen App">
       {rotations.map((rotation, rotKey) => {
@@ -23,13 +65,13 @@ function App() {
             key={rotKey}
             className="absolute origin-center"
             style={{
-              rotate: `${rotation}deg`,
+              rotate: `${-rotation * 3}deg`,
             }}
           >
             <div
               className="absolute flex"
               style={{
-                clipPath: `polygon(0 50%, 100% 0, 100% 100%)`,
+                // clipPath: `polygon(0 50%, 100% 0, 100% 100%)`,
                 overflow: "hidden",
               }}
             >
@@ -37,9 +79,9 @@ function App() {
                 return (
                   <div
                     key={itemKey}
-                    className={`w-5 ${
-                      selected.some((i) => i.rotation === rotKey && i.item === itemKey) ? "bg-red-500" : "bg-yellow-500"
-                    } border padding`}
+                    className={`relative w-5 h-6 ${
+                      selected.some((i) => i.rotation === rotKey && i.item === itemKey) ? "bg-red-500 z-50" : "bg-yellow-500 z-10"
+                    } ${itemKey < 1 ? "ml-48" : null} border padding`}
                     onClick={() => {
                       if (selected.some((i) => i.rotation === rotKey && i.item === itemKey)) {
                         console.log("remove");
@@ -50,7 +92,7 @@ function App() {
                       console.log(rotKey, itemKey, { rotation: rotKey, item: itemKey }, selected);
                     }}
                   >
-                    {String(item)}
+                    {String("")}
                   </div>
                 );
               })}
@@ -58,6 +100,8 @@ function App() {
           </div>
         );
       })}
+      <textarea className="absolute top-0 left-0 h-screen p-2 border w-96" value={JSON.stringify(bin1) + "\n\nhex: " + hex1} />
+      <textarea className="absolute top-0 right-0 h-screen p-2 border w-96" value={JSON.stringify(bin2) + "\n\nhex: " + hex2} />
     </div>
   );
 }
